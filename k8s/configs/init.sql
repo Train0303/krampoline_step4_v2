07f43b9a049190c2651db7202e6f939e2c76ce78
rjsdnxogh <rjsdnxogh@naver.com>
2023-10-30 10:58:38 +0900

    modi

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -1,5 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS `linknamu` DEFAULT CHARACTER SET utf8mb4;
 
-GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-GRANT ALL ON linknamu.* TO 'root'@'%';
+# GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON linknamu.* TO 'root'@'localhost';
 FLUSH PRIVILEGES;
