1227147fa7c99ee925e0bfa87898f0604895bab8
rjsdnxogh <rjsdnxogh@naver.com>
2023-10-30 12:09:47 +0900

    fix

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -1,5 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS `linknamu` DEFAULT CHARACTER SET utf8mb4;
 
-create user root@'%' identified by 'root'
+create user root@'%' identified by 'root';
 GRANT ALL ON linknamu.* TO root@'%';
 FLUSH PRIVILEGES;
