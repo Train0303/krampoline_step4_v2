fe9e79f69716a7996bd25506ef8b780d87400faa
rjsdnxogh <rjsdnxogh@naver.com>
2023-10-30 10:26:44 +0900

    modify

--- k8s/configs/init.sql
+++ k8s/configs/init.sql
@@ -1,7 +1,5 @@
 CREATE SCHEMA IF NOT EXISTS `linknamu` DEFAULT CHARACTER SET utf8mb4;
-
--- MySQL 8.0 이상에서는 CREATE USER와 ALTER USER를 사용하여 사용자를 생성하고 비밀번호를 설정합니다.
+​
 GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-GRANT ALL ON linknamu.* TO 'root'@'%';
-
+GRANT ALL ON linknamu.* TO 'root'@'localhost';
 FLUSH PRIVILEGES;
